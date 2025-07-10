04dd1618b5fae631bea8ffc60a601362831f43e7
Hannah <jmejanbarjuniper@gmail.com>
2025-07-10 09:42:12 -0800

    Add Tektur font

---
+++ code/include/fonts.hpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+ * fonts.hpp
+ * Solo header file
+ * Loads font files and stores them to global constants
+******************************************************************************/
+
+#ifndef FONTS_H
+    #define FONTS_H
+
+    #include <SFML/Graphics.hpp>
+
+    sf::Font TEKTUR("assets/fonts/Tektur-Regular.ttf");
+
+#endif
